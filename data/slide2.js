dd979f60f4565099b4b565df34c7e61539ae4124
karlivilca <147259587+karlivilca@users.noreply.github.com>
2023-10-07 09:45:43 -0500

    Add files via upload

---
+++ data/slide2.js
@@ -0,0 +1,62 @@
+(function(){var loadHandler=window['sl_{47BC3C15-DB03-4C93-95D1-C8642D2446D9}'];loadHandler&&loadHandler(1, '<div id="spr0_2c77ebd0"><div id="spr1_2c77ebd0" class="kern slide"><img id="img1_2c77ebd0" src="data/img2.png" width="720px" height="405px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_2c77ebd0" class="kern slide"><div id="spr3_2c77ebd0" style="left:40.665px;top:16.073px;"><div style="width:0px;"><span id="txt0_2c77ebd0" class="nokern" data-width="215.759995" style="left:211.46px;top:-4.307px;">Let’s start</span></div></div><div id="spr4_2c77ebd0" style="left:155.169px;top:83.152px;"><a id="hl0_2c77ebd0" href="http://www.youtube.com/watch?v=BoJBeGUHGv8" title="http://www.youtube.com/watch?v=BoJBeGUHGv8" target="_blank" onclick="document.getElementById(\'coreSpr_7858652\').getCore().processTriggerEffect(this);return document.getElementById(\'coreSpr_7858652\').getCore().gotoLink(this);"><img id="img0_2c77ebd0" src="data/img1.jpg" width="400" height="300" alt="Toy | Word Songs | Word Power | Pinkfong Songs for Children	Subscribe and watch new videos uploaded every week.\
+★ YouTube Channel: https://www.youtube.comhttp://www.youtube.com/Pinkfong\
+\
+PINKFONG! no. 1 kids\' app chosen by 100 million children worldwide\
+★ Best Kids Songs &amp; Stories [Free Download]: http://i.sstudy.kr/L/591/des/\
+\
+Learning words and names become easy with PINKFONG Word Songs.\
+\
+You are watching &quot;Toy&quot;, a super fun word song created by PINKFONG. \
+\
+\
+----\
+★ Lyrics\
+\
+\
+Toy\
+\
+\
+Bob bob bob. \
+Open the box, box. \
+Bob bob bob. \
+Open the box, box. \
+\
+\
+What, what’s in the box? in the box? What? What?\
+What, what’s in the box? in the box? What? What?\
+\
+\
+A doll. A ball. A robot. Blocks. Crayons. A truck!\
+\
+\
+A doll and a ball. A robot and a truck. \
+Blocks and crayons in the box. In the box!\
+A doll and a ball. A robot and a truck. \
+Blocks and crayons in the box. In the box!\
+Toy-oy-oy-oys are in the box. In the box!\
+\
+\
+Bob bob bob. Open the box, box. \
+Bob bob bob. Open the box, box. \
+Box! \
+\
+\
+Singer: Bommie Catherine Han, Hope Marie Segoine, Michael Yantzi, Anipen Matthew Digiacomo\
+Arranged by pinkfong, KizCastle   \
+\
+----\
+\
+\
+Subscribe to PINKFONG\'s YouTube channel for hundreds of kids\' favorite songs and stories, including phonics songs, nursery rhymes, bedtime lullabies, children\'s classics, fairy tales and more!\
+\
+\
+\
+Enjoy educational songs and stories for preschool kids created by the experts in children\'s education.\
+\
+\
+Follow us on Facebook for new updates and free promotions.\
+★ Facebook: https://www.facebook.com/PINKFONG.sma...\
+★ Website:  https://www.smartstudy.co.kr\
+\
+\
+Copyright © 2016 Smart Study Co., Ltd. All Rights Reserved." style="left:0.222px;top:0.166px;"/></a></div></div></div>', '{"s":[]}');})();
